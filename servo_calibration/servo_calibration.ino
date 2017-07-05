ad6de98c2e466fb0b7ab7c88be126daf3ff03cab
Baefoo <urimalsoft@gmail.com>
2017-07-05 09:05:52 +0900

    Language change
    korean -> english

--- servo_calibration/servo_calibration.ino
+++ servo_calibration/servo_calibration.ino
@@ -11,7 +11,7 @@ void setup() {
   command = 0;
   d_pin = 0;
 
-  Serial.println("Servo Calibration을 진행합니다.\n핀 번호를 세팅해주세요(예: p 3)");
+  Serial.println("This program calibrates the servo motor.\nYou must set the pin number(e.g: p 3).");
   printCommandList();
 }
 
@@ -38,29 +38,29 @@ void loop() {
 }
 
 void printCommandList() {
-  Serial.println("h:도움말\ni:현재 핀 번호 및 변경된 값\np:디지털 핀 세팅\n+:서보모터에 +값 주기\n-:서보모터에 -값 주기");
+  Serial.println("h:help\ni:current pin number & changed value\np:digital pin setting\n+: input + value\n-: input - value");
 }
 
 void printInfo() {
   Serial.println("================");
-  Serial.print("디지털 핀: ");
+  Serial.print("Digital pin: ");
   Serial.println(d_pin);
-  Serial.print("수정된 값: ");
+  Serial.print("Changed value: ");
   Serial.println(cali_count );
   Serial.println("================");
 }
 
 void setDigitalPin() {
-  Serial.print("디지털 핀 세팅(2~13): ");
+  Serial.print("Digital pin setting(2~13): ");
   int temp_pin = Serial.parseInt();
   if ( temp_pin >= 2 && temp_pin <= 13) {
     d_pin = temp_pin;
     servo.attach(d_pin);
-    Serial.print("디지털 핀이 수정되었습니다: ");
+    Serial.print("The digital pin has been modified.");
     Serial.println(d_pin);
     cali_count = 0;
   } else {
-    Serial.println("핀 번호 범위를 확인해주세요");
+    Serial.println("Please check the pin number.");
   }
 }
 
@@ -78,4 +78,3 @@ void calibration(char op) {
     }
   }
 }
-
